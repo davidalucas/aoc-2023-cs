a56ff782ed5e146cf09678490426a304f0964b1e
davidalucas <davidalucas92@gmail.com>
2023-12-30 15:09:07 -0500

    Implements the Calibrate function for Day 1 Part 2

--- AdventOfCode23.Tests/Day1/TrebuchetTests.cs
+++ AdventOfCode23.Tests/Day1/TrebuchetTests.cs
@@ -18,4 +18,66 @@ public class TrebuchetTests
         string[] data = File.ReadAllLines("Day1/data.txt");
         Trebuchet.Calibrate(data).Should().Be(55477);
     }
+
+
+    private readonly Dictionary<string, int> _numberMap = new()
+    {
+        { "zero", 0 },
+        { "one", 1 },
+        { "two", 2 },
+        { "three", 3 },
+        { "four", 4 },
+        { "five", 5 },
+        { "six", 6 },
+        { "seven", 7 },
+        { "eight", 8 },
+        { "nine", 9 }
+    };
+
+    [Fact]
+    public void TryForwardWordParse_Parses_First_Number()
+    {
+        var methodReturn = Trebuchet.TryForwardWordParse("two1nine", 0, _numberMap, out var methodResult);
+        methodReturn.Should().BeTrue();
+        methodResult.Should().Be(2);
+
+        methodReturn = Trebuchet.TryForwardWordParse("two1nine", 1, _numberMap, out methodResult);
+        methodReturn.Should().BeFalse();
+        methodResult.Should().Be(0);
+    }
+
+    [Fact]
+    public void TryReverseWordParse_Parses_Last_Number()
+    {
+        var methodReturn = Trebuchet.TryReverseWordParse("two1nine", 7, _numberMap, out var methodResult);
+        methodReturn.Should().BeTrue();
+        methodResult.Should().Be(9);
+
+        methodReturn = Trebuchet.TryReverseWordParse("two1nine", 6, _numberMap, out methodResult);
+        methodReturn.Should().BeFalse();
+        methodResult.Should().Be(0);
+    }
+
+    [Fact]
+    public void Calibrate_Calculates_With_Word_Search_For_Example_Data()
+    {
+        string[] data =
+        [
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen",
+        ];
+        Trebuchet.Calibrate(data, _numberMap).Should().Be(281);
+    }
+    
+    [Fact]
+    public void Calibrate_Calculates_With_Word_Search_For_Real_Data()
+    {
+        string[] data = File.ReadAllLines("Day1/data.txt");
+        Trebuchet.Calibrate(data, _numberMap).Should().Be(54431);
+    }
 }
--- AdventOfCode23/Day1/Trebuchet.cs
+++ AdventOfCode23/Day1/Trebuchet.cs
@@ -16,7 +16,7 @@ public static class Trebuchet
         return lines.Select(val =>
         {
             if (val == string.Empty) return 0;
-            
+
             var a = 0;
             var b = 0;
 
@@ -47,5 +47,91 @@ public static class Trebuchet
 
             return a * 10 + b;
         }).Aggregate((a, b) => a + b);
+    }
+
+    /// <summary>
+    /// Performs the enhanced calibration from the AoC Day 1 Part 2 problem.
+    /// </summary>
+    /// <param name="lines">The data to parse in order to calculate the calibration factor.</param>
+    /// <param name="numMap">The number map to use for parsing out numbers represented as words (i.e. "one" for 1).</param>
+    /// <returns>The calculated calibration factor.</returns>
+    public static int Calibrate(IEnumerable<string> lines, Dictionary<string, int> numMap)
+    {
+        var lineArr = lines.ToArray();
+        var coordinates = new int[lineArr.Length];
+
+        var a = 0;
+        var b = 0;
+
+        for (var i = 0; i < lineArr.Length; i++)
+        {
+            for (var j = 0; j < lineArr[i].Length; j++)
+            {
+                if (int.TryParse(lineArr[i][j].ToString(), out a)) break;
+                if (TryForwardWordParse(lineArr[i], j, numMap, out a)) break;
+            }
+
+            for (var j = lineArr[i].Length - 1; j >= 0; j--)
+            {
+                if (int.TryParse(lineArr[i][j].ToString(), out b)) break;
+                if (TryReverseWordParse(lineArr[i], j, numMap, out b)) break;
+            }
+
+            coordinates[i] = a * 10 + b;
+            a = b = 0;
+        }
+
+        return coordinates.Aggregate((i, j) => i + j);
+    }
+
+    /// <summary>
+    ///     Performs the (forward) word number parsing for the Day 1 Part 2 algorithm. This implementation tries to emulate the
+    ///     .NET int.TryParse method to make the implementation in Calibrate look a little more cohesive.
+    /// </summary>
+    /// <param name="line"></param>
+    /// <param name="currIdx"></param>
+    /// <param name="numMap"></param>
+    /// <param name="result"></param>
+    /// <returns>A bool value indicating whether a result has been found</returns>
+    public static bool TryForwardWordParse(string line, int currIdx, Dictionary<string, int> numMap, out int result)
+    {
+        // loop over each item in the numMap
+        foreach (var kvPair in numMap)
+        {
+            if (currIdx < 0 || currIdx + kvPair.Key.Length > line.Length) continue;
+            var subStr = line.Substring(currIdx, kvPair.Key.Length);
+            if (subStr != kvPair.Key) continue;
+            result = kvPair.Value;
+            return true;
+        }
+
+        result = 0;
+        return false;
+    }
+    
+    /// <summary>
+    ///     Performs the (reverse) word number parsing for the Day 1 Part 2 algorithm. This implementation tries to emulate the
+    ///     .NET int.TryParse method to make the implementation in Calibrate look a little more cohesive.
+    /// </summary>
+    /// <param name="line"></param>
+    /// <param name="currIdx"></param>
+    /// <param name="numMap"></param>
+    /// <param name="result"></param>
+    /// <returns>A bool value indicating whether a result has been found</returns>
+    public static bool TryReverseWordParse(string line, int currIdx, Dictionary<string, int> numMap, out int result)
+    {
+        // loop over each item in the numMap
+        foreach (var kvPair in numMap)
+        {
+            int startIdx = currIdx - kvPair.Key.Length + 1;
+            if (startIdx < 0 || startIdx + kvPair.Key.Length > line.Length) continue;
+            var subStr = line.Substring(startIdx, kvPair.Key.Length);
+            if (subStr != kvPair.Key) continue;
+            result = kvPair.Value;
+            return true;
+        }
+
+        result = 0;
+        return false;
     }
 }
