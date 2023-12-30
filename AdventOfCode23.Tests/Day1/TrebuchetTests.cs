7381a28d743df0d5120ed42f810c2058f8d7128d
davidalucas <davidalucas92@gmail.com>
2023-12-30 13:34:50 -0500

    Implements Calibrate function and sample data test

--- AdventOfCode23.Tests/Day1/TrebuchetTests.cs
+++ AdventOfCode23.Tests/Day1/TrebuchetTests.cs
@@ -1,9 +1,14 @@
+using AdventOfCode23.Day1;
+using FluentAssertions;
+
 namespace AdventOfCode23.Tests.Day1;
 
 public class TrebuchetTests
 {
     [Fact]
-    public void Test1()
+    public void Calibrate_Calculates_Example_Data()
     {
+        string[] data = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+        Trebuchet.Calibrate(data).Should().Be(142);
     }
 }
--- AdventOfCode23/Day1/Trebuchet.cs
+++ AdventOfCode23/Day1/Trebuchet.cs
@@ -1,5 +1,51 @@
 ﻿namespace AdventOfCode23.Day1;
 
-public class Trebuchet
+public static class Trebuchet
 {
+    /// <summary>
+    ///     Performs the calibration specified in the AoC Day 1 Part 1 problem.
+    /// </summary>
+    /// <param name="lines">The lines to parse through.</param>
+    /// <returns>The calibration value.</returns>
+    /// <remarks>
+    ///     In this implementation, we're going to try a LINQ-only implementation. We'll go for better performance on the
+    ///     Part 2 algorithm.
+    /// </remarks>
+    public static int Calibrate(IEnumerable<string> lines)
+    {
+        return lines.Select(val =>
+        {
+            if (val == string.Empty) return 0;
+            
+            var a = 0;
+            var b = 0;
+
+            try
+            {
+                a = int.Parse(
+                    val.First(c => int.TryParse(c.ToString(), out _))
+                        .ToString()
+                );
+            }
+            catch
+            {
+                // ignored
+            }
+
+            try
+            {
+                b = int.Parse(
+                    val.Reverse()
+                        .First(c => int.TryParse(c.ToString(), out _))
+                        .ToString()
+                );
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return a * 10 + b;
+        }).Aggregate((a, b) => a + b);
+    }
 }
