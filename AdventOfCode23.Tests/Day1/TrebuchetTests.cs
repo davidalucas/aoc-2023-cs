ed6f49ba91ff35e16e38bc00dfe2f73ea049c75f
davidalucas <davidalucas92@gmail.com>
2023-12-30 13:39:18 -0500

    Implements the Calibrate function test with the real data

--- AdventOfCode23.Tests/Day1/TrebuchetTests.cs
+++ AdventOfCode23.Tests/Day1/TrebuchetTests.cs
@@ -11,4 +11,11 @@ public class TrebuchetTests
         string[] data = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
         Trebuchet.Calibrate(data).Should().Be(142);
     }
+
+    [Fact]
+    public void Calibrate_Calculates_Real_Data()
+    {
+        string[] data = File.ReadAllLines("Day1/data.txt");
+        Trebuchet.Calibrate(data).Should().Be(55477);
+    }
 }
