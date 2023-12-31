d0445f32b324edd98af33d43a58d22708151f813
davidalucas <davidalucas92@gmail.com>
2023-12-31 17:31:03 -0500

    Adds the CubeGame and Reveal classes

---
+++ AdventOfCode23/Day2/CubeGame.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode23.Day2;
+
+public class CubeGame(int gameNumber, params Reveal[] reveals)
+{
+    public int GameNumber { get; } = gameNumber;
+    public Reveal[] Reveals { get; } = reveals;
+
+    /// <summary>
+    ///     Checks whether the game is possible with the given constraints
+    /// </summary>
+    /// <param name="red">The maximum number of red cubes allowed.</param>
+    /// <param name="green">The maximum number of green cubes allowed.</param>
+    /// <param name="blue">The maximum number of blue cubes allowed.</param>
+    /// <returns><see langword="true"/> if the game is possible with the given constraints.</returns>
+    public bool IsPossible(int red, int green, int blue)
+    {
+        foreach (var reveal in Reveals)
+        {
+            if (reveal.Reds > red || reveal.Greens > green || reveal.Blues > blue)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ AdventOfCode23/Day2/Reveal.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode23.Day2;
+
+public class Reveal(int reds, int greens, int blues)
+{
+    public int Reds { get; } = reds;
+    public int Greens { get; } = greens;
+    public int Blues { get; } = blues;
+}
