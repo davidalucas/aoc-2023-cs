41d5ece1706e8dc4926cc8a91669f9e3ea29f072
davidalucas <davidalucas92@gmail.com>
2024-01-04 21:15:06 -0500

    Implements the algorithm (and tests) for Day 3 Part 1 problem

---
+++ AdventOfCode23.Tests/Day3/SchematicTests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode23.Day3;
+using FluentAssertions;
+
+namespace AdventOfCode23.Tests.Day3;
+
+public class SchematicTests
+{
+    [Fact]
+    public void SumAllPartNumbers_Returns_Correct_Sum_From_Example()
+    {
+        var data = File.ReadAllLines("Day3/example.txt");
+        Schematic.SumAllPartNumbers(data).Should().Be(4361);
+    }
+
+    [Fact]
+    public void SumAllPartNumbers_Returns_Correct_Sum_From_Actual_Data()
+    {
+        var data = File.ReadAllLines("Day3/data.txt");
+        Schematic.SumAllPartNumbers(data).Should().Be(544664);
+    }
+}
