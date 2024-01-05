4d504053fb5ea502ebeda6d9a1466cc073840320
davidalucas <davidalucas92@gmail.com>
2024-01-05 12:40:22 -0500

    Implemented Day 5 Part 2 algorithm and tests
    Adds PartNumber and Gear classes to help make the code a little easier to understand

---
+++ AdventOfCode23/Day3/PartNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode23.Day3;
+
+public class PartNumber : IEquatable<PartNumber>
+{
+    public int Value { get; set; }
+    public int LineFound { get; set; }
+    public int LinePositionStart { get; set; }
+    public int LinePositionEnd { get; set; }
+
+    public PartNumber(string[] rawData, int lineFound, int linePositionStart, int linePositionEnd)
+    {
+        LineFound = lineFound;
+        LinePositionStart = linePositionStart;
+        LinePositionEnd = linePositionEnd;
+        Value = int.Parse(rawData[lineFound].Substring(linePositionStart, linePositionEnd - linePositionStart + 1));
+    }
+
+    public bool Equals(PartNumber? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return other.Value == Value && other.LineFound == LineFound && other.LinePositionStart == LinePositionStart &&
+               other.LinePositionEnd == LinePositionEnd;
+    }
+}
