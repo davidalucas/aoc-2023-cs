b584ff739d2aac8c2d0f00e149d41e69849d4b14
davidalucas <davidalucas92@gmail.com>
2024-01-06 12:08:13 -0500

    Adds the AlmanacMap class and test for the constructor

---
+++ AdventOfCode23.Tests/Day5/AlmanacMapTests.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode23.Day5;
+using FluentAssertions;
+
+namespace AdventOfCode23.Tests.Day5;
+
+public class AlmanacMapTests
+{
+    [Fact]
+    public void AlmanacMapConstructor_Constructs_Correctly()
+    {
+        var map = new AlmanacMap("50 98 2");
+        map.Source.Should().Be(98);
+        map.Destination.Should().Be(50);
+        map.Range.Should().Be(2);
+    }
+}
---
+++ AdventOfCode23/Day5/AlmanacMap.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode23.Day5;
+
+public class AlmanacMap
+{
+    public int Source { get; set; }
+    public int Destination { get; set; }
+    public int Range { get; set; }
+
+    public AlmanacMap(string data)
+    {
+        var splitData = data.Trim().Split(" ");
+        Source = int.Parse(splitData[1]);
+        Destination = int.Parse(splitData[0]);
+        Range = int.Parse(splitData[2]);
+    }
+}
