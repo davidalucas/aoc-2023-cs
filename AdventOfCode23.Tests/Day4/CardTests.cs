bf0157516deb0a4c554c7c6857c7100d45e846a2
davidalucas <davidalucas92@gmail.com>
2024-01-05 21:14:15 -0500

    Implements the CalculateCards function
    Does the tricky work for this exercise

--- AdventOfCode23.Tests/Day4/CardTests.cs
+++ AdventOfCode23.Tests/Day4/CardTests.cs
@@ -18,8 +18,27 @@ public class CardTests
     [Fact]
     public void CalculateScore_Calculates_Correct_Score()
     {
-        
         var score = Card.CalculateScore("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1");
         score.Should().Be(2);
+    }
+
+    [Fact]
+    public void CalculateCards_Calculates_Correct_Sum_And_List()
+    {
+        LinkedList<int> memory = [];
+        var result = Card.CalculateCards("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", memory);
+
+        result.Should().Be(1);
+        memory.Should().BeEquivalentTo([1, 1, 1, 1]);
+
+        result = Card.CalculateCards("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", memory);
+
+        result.Should().Be(2);
+        memory.Should().BeEquivalentTo([3, 3, 1]);
+        
+        result = Card.CalculateCards("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", memory);
+
+        result.Should().Be(4);
+        memory.Should().BeEquivalentTo([7, 5]);
     }
 }
